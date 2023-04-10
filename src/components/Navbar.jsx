742ed69b7454b39643c41781d89cb4cd229b09b7
BeinerthChitiva <beinerthps4@gmail.com>
2023-04-10 18:07:37 -0500

    Commit#2

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,31 +1,28 @@
-import "./Navbar.css"
+import "./Navstyles.css"
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faAlignJustify  } from '@fortawesome/free-solid-svg-icons'
 
 
 function Navbar(){
     return(
-        <nav className="navbar">
-            <div className="container">
-                <div className="nav-container">
-                    <div className="nav-logo">
-                        <h3>Beinerth Chitiva M.</h3>
-                    </div>
-                    <div className="nav-links">
-                        <ul>
-                            <li>
-                                <a href="#home">Home</a>
-                            </li>
-                            <li>
-                                <a href="#about">About</a>
-                            </li>
-                            <li>
-                                <a href="#projects">Projects</a>
-                            </li>
-                            <li>
-                                <a href="#contact">Contact</a>
-                            </li>
-                        </ul>
-                    </div>
-                </div>
+        <nav>
+            <h3 className="logo">Beinerth Chitiva M.</h3>
+            <ul>
+                <li>
+                    <a href="#home">Home</a>
+                </li>
+                <li>
+                    <a href="#about">About</a>
+                </li>
+                <li>
+                    <a href="#projects">Projects</a>
+                </li>
+                <li>
+                    <a href="#contact">Contact</a>
+                </li>
+            </ul>
+            <div className="mobile-menu">
+                <FontAwesomeIcon icon={faAlignJustify}/>
             </div>
         </nav>
     )
